3001c1f6e53d88c63693d9db9e4db9cf0b800957
Ben Holmes <ben.holmes@hashicorp.com>
2025-06-29 23:12:40 +0100

    fix kvv2

--- vault_secrets_plugin/static_backend.py
+++ vault_secrets_plugin/static_backend.py
@@ -1,26 +1,34 @@
 import json
 import requests
 
+import json
+import requests
+
 def get_static_secret(kwargs, headers):
     url = kwargs["url"].rstrip("/")
     mount = kwargs["mount"]
     path = kwargs["path"]
-    version = kwargs.get("version")
+    kv_version = kwargs.get("kv_version", "2")  # Default to KV v2
     secret_key = kwargs.get("secret_key")
+    version = kwargs.get("version")
 
-    if version:
+    if kv_version == "2":
         full_url = f"{url}/v1/{mount}/data/{path}"
-    else:
+        params = {}
+        if version:
+            params["version"] = version
+        resp = requests.get(full_url, headers=headers, params=params)
+        resp.raise_for_status()
+        secret_data = resp.json()["data"]["data"]
+
+    elif kv_version == "1":
         full_url = f"{url}/v1/{mount}/{path}"
+        resp = requests.get(full_url, headers=headers)
+        resp.raise_for_status()
+        secret_data = resp.json()["data"]
 
-    resp = requests.get(full_url, headers=headers)
-    resp.raise_for_status()
-    data = resp.json()
-
-    if version:
-        secret_data = data["data"]["data"]
     else:
-        secret_data = data["data"]
+        raise ValueError(f"Unsupported kv_version: {kv_version}")
 
     if secret_key:
         return {"secret_value": secret_data[secret_key]}
