60b212de01a26d99efaf87ddae58f026f1c27d78
Ben Holmes <ben.holmes@hashicorp.com>
2025-06-30 11:06:36 +0100

    updated install script

--- setup/install_vault_plugin.sh
+++ setup/install_vault_plugin.sh
@@ -5,6 +5,8 @@ echo "Installing Vault Secrets Plugin into awx-python environment..."
 pushd ../
 awx-python -m pip install .
 
+awx-manage setup_managed_credential_types
+
 echo "Restarting AWX services..."
 automation-controller-service restart
 popd
