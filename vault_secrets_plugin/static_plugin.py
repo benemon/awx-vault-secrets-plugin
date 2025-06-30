db604f5c03c8043c5e43fcbf5c105cf59c3930e2
Ben Holmes <ben.holmes@hashicorp.com>
2025-06-30 12:04:16 +0100

    reset implementation

--- vault_secrets_plugin/static_plugin.py
+++ vault_secrets_plugin/static_plugin.py
@@ -1,15 +1,27 @@
 name = "HashiCorp Vault Static Secret"
 
+
+metadata = {
+    "version": "0.1.0",
+    "author": "Benjamin Holmes"
+}
+
 inputs = {
     "fields": [
-        {
-            "id": "url",
-            "label": "Vault URL",
-            "type": "string"
-        }
+        {"id": "url", "label": "Vault URL", "type": "string"},
+        {"id": "namespace", "label": "Vault Namespace", "type": "string"},
+        {"id": "token", "label": "Token", "type": "string"},
+        {"id": "role_id", "label": "AppRole Role ID", "type": "string"},
+        {"id": "secret_id", "label": "AppRole Secret ID", "type": "string"},
+        {"id": "jwt", "label": "JWT Token", "type": "string"},
+        {"id": "jwt_role", "label": "JWT Role", "type": "string"},
+        {"id": "mount", "label": "Mount Point", "type": "string"},
+        {"id": "path", "label": "Secret Path", "type": "string"},
+        {"id": "secret_key", "label": "Secret Key", "type": "string"}
     ],
-    "required": ["url"]
+    "required": ["url", "mount", "path"]
 }
+
 
 injectors = {
     "env": {
