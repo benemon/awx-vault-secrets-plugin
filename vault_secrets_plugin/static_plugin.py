e6c8af53a3fed828f1d728b3e84f0a22fc51ecc3
Ben Holmes <ben.holmes@hashicorp.com>
2025-06-30 12:08:15 +0100

    reset implementation

--- vault_secrets_plugin/static_plugin.py
+++ vault_secrets_plugin/static_plugin.py
@@ -10,6 +10,11 @@ inputs = {
     "fields": [
         {"id": "url", "label": "Vault URL", "type": "string"},
         {"id": "namespace", "label": "Vault Namespace", "type": "string"},
+        {"id": "auth_method", "label": "Auth Method", "type": "string", "choices": [
+            {"label": "Token", "value": "token"},
+            {"label": "AppRole", "value": "approle"},
+            {"label": "JWT", "value": "jwt"}
+        ]},
         {"id": "token", "label": "Token", "type": "string"},
         {"id": "role_id", "label": "AppRole Role ID", "type": "string"},
         {"id": "secret_id", "label": "AppRole Secret ID", "type": "string"},
@@ -19,7 +24,7 @@ inputs = {
         {"id": "path", "label": "Secret Path", "type": "string"},
         {"id": "secret_key", "label": "Secret Key", "type": "string"}
     ],
-    "required": ["url", "mount", "path"]
+    "required": ["url", "auth_method", "mount", "path"]
 }
 
 
