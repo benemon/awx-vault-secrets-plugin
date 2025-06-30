9bcdbb8f8addb303ddecd24b687ae2b3f2b1303a
Ben Holmes <ben.holmes@hashicorp.com>
2025-06-30 11:57:40 +0100

    reset implementation

--- setup.py
+++ setup.py
@@ -1,16 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="awx-vault-secrets-plugin",
+    name="vault-secrets-static-plugin",
     version="0.1.0",
     packages=find_packages(),
-    zip_safe=False,
     entry_points={
         "awx.credential_plugins": [
-            "hashicorp_vault_static_secret = vault_secrets_plugin.static_plugin"
+            "hashicorp_vault_static_secret = vault_secrets_plugin.vault_static_plugin"
         ]
     },
-    install_requires=[
-        "requests"
-    ]
+    install_requires=[],
 )
