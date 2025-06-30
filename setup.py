d4ea941e81d662766d4220e687c1b7a987843abc
Ben Holmes <ben.holmes@hashicorp.com>
2025-06-30 11:14:47 +0100

    updated setup metadata

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="awx-vault-secrets-plugin",
-    version="0.0.1",
-    packages=["vault_secrets_plugin"],
+    version="0.1.0",
+    packages=find_packages(),
+    zip_safe=False,
     entry_points={
         "awx.credential_plugins": [
             "hashicorp_vault_static_secret = vault_secrets_plugin.static_plugin",
             "hashicorp_vault_dynamic_secret = vault_secrets_plugin.dynamic_plugin"
         ]
     },
-    install_requires=["requests"]
+    install_requires=[
+        "requests"
+    ]
 )
