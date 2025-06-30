ba690c288376113e17711fc658bee474140101e3
Ben Holmes <ben.holmes@hashicorp.com>
2025-06-30 12:12:34 +0100

    reset implementation

--- vault_secrets_plugin/static_plugin.py
+++ vault_secrets_plugin/static_plugin.py
@@ -6,35 +6,19 @@ metadata = {
     "author": "Benjamin Holmes"
 }
 
-inputs = {
-    "fields": [
-        {"id": "url", "label": "Vault URL", "type": "string"},
-        {"id": "namespace", "label": "Vault Namespace", "type": "string"},
-        {"id": "auth_method", "label": "Auth Method", "type": "string", "choices": [
-            {"label": "Token", "value": "token"},
-            {"label": "AppRole", "value": "approle"},
-            {"label": "JWT", "value": "jwt"}
-        ]},
-        {"id": "token", "label": "Token", "type": "string"},
-        {"id": "role_id", "label": "AppRole Role ID", "type": "string"},
-        {"id": "secret_id", "label": "AppRole Secret ID", "type": "string"},
-        {"id": "jwt", "label": "JWT Token", "type": "string"},
-        {"id": "jwt_role", "label": "JWT Role", "type": "string"},
-        {"id": "mount", "label": "Mount Point", "type": "string"},
-        {"id": "path", "label": "Secret Path", "type": "string"},
-        {"id": "secret_key", "label": "Secret Key", "type": "string"}
-    ],
-    "required": ["url", "auth_method", "mount", "path"]
-}
-
+# No fields in the plugin
+inputs = {}
 
 injectors = {
     "env": {
-        "VAULT_SECRET_JSON": "{{ secret_json }}"
+        "VAULT_SECRET_JSON": "{{ secret_json }}",
+        "VAULT_SECRET_VALUE": "{{ secret_value }}"
     }
 }
 
 def backend(**kwargs):
+    # Return dummy data for now
     return {
-        "secret_json": '{"dummy":"value"}'
+        "secret_json": '{"dummy":"value"}',
+        "secret_value": "dummy_secret"
     }
